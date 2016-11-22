61805a34364e31786f35b91428af0a9d645783c7
hhhhuy112 <minhhuyho2011@gmail.com>
2016-11-22 14:14:21 +0700

    finised user-followed-followin

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
 
   delete 'logout' => 'sessions#destroy'
   
-  resources :users
+   resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :entries 
+   resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
